8d4d952b8ca4ccf9f3d105ea5355342b4e2eba69
zwang-bidmc <150841846+zwang-bidmc@users.noreply.github.com>
2023-11-23 00:13:35 -0500

    update example code

--- R/flex_ewas.R
+++ R/flex_ewas.R
@@ -33,7 +33,7 @@
 #' # Create a robust linear regression function with methylation as exposure
 #' covariates_string = c('age','sex')
 #' rlm_function <- function(x) {
-#' mod <- base::suppressWarnings(MASS::rlm(as.formula(paste('body_fat', "~x+", covariates_string),data = phenoData)))
+#' mod <- base::suppressWarnings(rlm(as.formula(paste('height', "~x+", 'age')), data = phenoData))
 #' return(summary(mod)$coefficients[2,])
 #' }
 #'
